a76287381a83dedf927e007850d7297294d5c2e1
ucfninf <ucfninf@ucl.ac.uk>
2024-03-28 23:35:04 +0000

    add new page

---
+++ lib/home-screen.dart
@@ -0,0 +1,3 @@
+// ignore: file_names, unused_import
+import 'package:flutter/material.dart';
+
--- lib/main.dart
+++ lib/main.dart
@@ -75,4 +75,3 @@ class RandomWordsState extends State<RandomWords> {
   }
                                         
 }
-
