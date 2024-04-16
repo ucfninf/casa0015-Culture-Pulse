051a667d1266445dbab7648b8dc77ae715861599
ucfninf <ucfninf@ucl.ac.uk>
2024-04-16 19:14:41 +0100

    map page with geolocator

--- Culture_Pulse_app/lib/app_page/map_page.dart
+++ Culture_Pulse_app/lib/app_page/map_page.dart
@@ -1,1 +1,52 @@
-//will attach google API here
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+//use StatefulWidget pattern to manage the state 
+//of markers on the map and the user's current location.
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MapPage>
+    with AutomaticKeepAliveClientMixin<MapPage> {
+  LatLng? currentPosition;
+  List<EveryScenicEntity> scenicList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    scenicList = AllScenic.scenicList;
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      /// Request permission first
+      await _getCurrentLocation();
+      setState(() {});
+    });
+  }
+
+  // Get current location (here is to double check permissions)
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      print('Location services are disabled.');
+      return;
+    }
+
+// Get location permissions
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Location permissions are denied (actual value: $permission).');
+        return;
+      }
+    }
+
+if (
