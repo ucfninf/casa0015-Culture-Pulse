d129462c3569cca78394783097b95a82acff7dc9
ucfninf <ucfninf@ucl.ac.uk>
2024-04-16 19:20:06 +0100

    different colored marker icons according to types of attractions

--- Culture_Pulse_app/lib/app_page/map_page.dart
+++ Culture_Pulse_app/lib/app_page/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:geolocator/geolocator.dart';
 
 //use StatefulWidget pattern to manage the state 
@@ -55,7 +56,7 @@ class _MyHomePageState extends State<MapPage>
       return;
     }
 
-
+ // Get current location
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
@@ -63,3 +64,36 @@ class _MyHomePageState extends State<MapPage>
       currentPosition = LatLng(position.latitude, position.longitude);
     });
   }
+
+  BitmapDescriptor getIcon(int type) {
+    switch (type) {
+      /// Mine
+      case -1:
+        return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue);
+
+      /// Museum
+      case 0:
+        return BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueOrange);
+
+      /// Park
+      case 1:
+        return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
+
+      /// Historical Heritage site
+      case 2:
+        return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed);
+      case 3:
+        return BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueMagenta);
+
+      /// View Point
+      case 4:
+        return BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan);
+      default:
+        return BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueYellow);
+    }
+  }
+
+  @SemanticsHintOverrides()
