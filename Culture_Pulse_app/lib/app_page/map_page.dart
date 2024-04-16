230ed2ade83c67215894d5f48c95fd9f22a4750e
ucfninf <ucfninf@ucl.ac.uk>
2024-04-16 19:23:34 +0100

    upload google API key, customized database

--- Culture_Pulse_app/lib/app_page/map_page.dart
+++ Culture_Pulse_app/lib/app_page/map_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:culture_pulse_app/cache.dart';
+import 'package:culture_pulse_app/custom_database/database.dart';
+import 'package:culture_pulse_app/model/every_scenic_entity.dart';
 //use StatefulWidget pattern to manage the state 
 //of markers on the map and the user's current location.
 
@@ -96,4 +98,126 @@ class _MyHomePageState extends State<MapPage>
     }
   }
 
-  @SemanticsHintOverrides()
+ @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+
+      appBar: AppBar(
+        leading: null,
+        automaticallyImplyLeading: false,
+        backgroundColor:
+            Color(SpUtil.getInt(Cache.mainColorCache) ?? Cache.mainColor.value),
+        centerTitle: true,
+        title: const Text(
+          'Map Page',
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  /// The reason for adding restrictions and sliding here is that the
+                  /// Google Map SDK will automatically redraw when the keyboard pops up.
+                  /// Its markers and the image cache queue used for image processing will be destroyed during redrawing and making zoom unfunctional
+                  width: MediaQuery.sizeOf(context).width,
+                  height: MediaQuery.sizeOf(context).height - 170,
+                  child: GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      /// Get current location
+                      target: currentPosition ?? const LatLng(51.5074, -0.1278),
+                      zoom: 10,
+                    ),
+                    myLocationButtonEnabled: true,
+                    markers: {
+                      ...List.generate(
+                          scenicList.length,
+                          (index) => Marker(
+                                markerId: MarkerId(scenicList[index].id.toString()),
+                                position: LatLng(scenicList[index].latitude!,
+                                    scenicList[index].longitude!),
+                                onTap: () {
+                                  /// Jump to attraction details page
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => MapDetailPage(
+                                                scenicModel: scenicList[index],
+                                              )));
+                                },
+                                icon: getIcon(scenicList[index].type!),
+                                infoWindow:
+                                    InfoWindow(title: scenicList[index].name ?? ''),
+                              )),
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              top: 10,
+              right: 10,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    height: 50,
+                    width: 200,
+                    child: TextField(
+                      controller: TextEditingController(),
+                      focusNode: null,
+                      readOnly: true,
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const SearchScreen()));
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Search...',
+                        prefixIcon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                  // getSymbolStyle(Colors.blue,'My Location'),
+                  getSymbolStyle(Colors.orange, 'Museum'),
+                  getSymbolStyle(Colors.green, 'Park'),
+                  getSymbolStyle(Colors.red, 'Historical Site'),
+                  getSymbolStyle(Colors.cyan, 'Scenic spot'),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget getSymbolStyle(Color symbolColor, String title) {
+    return Container(
+      width: 160,
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Icon(
+            Icons.location_on,
+            color: symbolColor,
+          ),
+          const Expanded(
+            child: SizedBox(
+              width: 10,
+            ),
+          ),
+          Text(title)
+        ],
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
