3ceedc2e3d19d5a9a3a079c2670bed8baca9022e
ucfninf <ucfninf@ucl.ac.uk>
2024-04-22 12:59:58 +0100

    update

--- Culture_Pulse_app/lib/main.dart
+++ Culture_Pulse_app/lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:culture_pulse_app/cache.dart';
 import 'package:culture_pulse_app/app_page/splash_page.dart';
 import 'package:sp_util/sp_util.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 
@@ -18,43 +15,13 @@ void main() async {
   if (SpUtil.getInt(Cache.mainColorCache) == 0) {
     SpUtil.putInt(Cache.mainColorCache, Cache.mainColor.value);
   }
-  await initializeMapRenderer();
   runApp(const MyApp());
 }
-
-Completer<AndroidMapRenderer?>? _initializedRendererCompleter;
-
-bool _init = false;
 
 /// Initializes map renderer to the `latest` renderer type for Android platform.
 ///
 /// The renderer must be requested before creating GoogleMap instances,
 /// as the renderer can be initialized only once per application context.
-Future<AndroidMapRenderer?> initializeMapRenderer() async {
-  if (!_init) {
-    if (_initializedRendererCompleter != null) {
-      return _initializedRendererCompleter!.future;
-    }
-
-    final Completer<AndroidMapRenderer?> completer =
-        Completer<AndroidMapRenderer?>();
-    _initializedRendererCompleter = completer;
-
-    final GoogleMapsFlutterPlatform mapsImplementation =
-        GoogleMapsFlutterPlatform.instance;
-    if (mapsImplementation is GoogleMapsFlutterAndroid) {
-      unawaited(mapsImplementation
-          .initializeWithRenderer(AndroidMapRenderer.latest)
-          .then((AndroidMapRenderer initializedRenderer) =>
-              completer.complete(initializedRenderer)));
-    } else {
-      completer.complete(null);
-    }
-    _init = true;
-    return completer.future;
-  }
-  return null;
-}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
